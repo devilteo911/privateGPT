1761944b0a2dd756a27543768cc20001ef21f4e2
Matteo Moro <morom@previdom.previnet.it>
2023-06-07 15:26:09 +0000

    ✨ italian custom prompt + gpu

--- privateGPT.py
+++ privateGPT.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+
+# LLAMA_CUBLAS=1 CMAKE_ARGS=-DLLAMA_CUBLAS=on FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir --force-reinstall --verbose
+
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
+from langchain.prompts import PromptTemplate
 from langchain.llms import GPT4All, LlamaCpp
 import os
 import argparse
@@ -11,33 +15,68 @@ import argparse
 load_dotenv()
 
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
+persist_directory = os.environ.get("PERSIST_DIRECTORY")
 
-model_type = os.environ.get('MODEL_TYPE')
-model_path = os.environ.get('MODEL_PATH')
-model_n_ctx = os.environ.get('MODEL_N_CTX')
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+model_type = os.environ.get("MODEL_TYPE")
+model_path = os.environ.get("MODEL_PATH")
+model_n_ctx = os.environ.get("MODEL_N_CTX")
+target_source_chunks = int(os.environ.get("TARGET_SOURCE_CHUNKS", 4))
 
 from constants import CHROMA_SETTINGS
+
 
 def main():
     # Parse the command line arguments
     args = parse_arguments()
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+    db = Chroma(
+        persist_directory=persist_directory,
+        embedding_function=embeddings,
+        client_settings=CHROMA_SETTINGS,
+    )
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
     # activate/deactivate the streaming StdOut callback for LLMs
     callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
     # Prepare the LLM
     match model_type:
         case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)
+            llm = LlamaCpp(
+                model_path=model_path,
+                n_ctx=model_n_ctx,
+                callbacks=callbacks,
+                verbose=False,
+                temperature=0.0,
+                n_gpu_layers=40,
+            )
         case "GPT4All":
-            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
+            llm = GPT4All(
+                model=model_path,
+                n_ctx=model_n_ctx,
+                backend="gptj",
+                callbacks=callbacks,
+                verbose=False,
+            )
         case _default:
             print(f"Model {model_type} not supported!")
-            exit;
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
+            exit
+    prompt_template = """Use the following pieces of context to answer the question at 
+    the end. If you don't know the answer, just say that you don't know, 
+    don't try to make up an answer.
+
+    {context}
+
+    Question: {question}
+    Answer in Italian:"""
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=not args.hide_source,
+        chain_type_kwargs={"prompt": PROMPT},
+    )
     # Interactive questions and answers
     while True:
         query = input("\nEnter a query: ")
@@ -46,7 +85,10 @@ def main():
 
         # Get the answer from the chain
         res = qa(query)
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
+        answer, docs = (
+            res if args.hide_source else res["result"],
+            [] if args.hide_source else res["source_documents"],
+        )
 
         # Print the result
         print("\n\n> Question:")
@@ -59,15 +101,25 @@ def main():
             print("\n> " + document.metadata["source"] + ":")
             print(document.page_content)
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
-                                                 'using the power of LLMs.')
-    parser.add_argument("--hide-source", "-S", action='store_true',
-                        help='Use this flag to disable printing of source documents used for answers.')
 
-    parser.add_argument("--mute-stream", "-M",
-                        action='store_true',
-                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="privateGPT: Ask questions to your documents without an internet connection, "
+        "using the power of LLMs."
+    )
+    parser.add_argument(
+        "--hide-source",
+        "-S",
+        action="store_true",
+        help="Use this flag to disable printing of source documents used for answers.",
+    )
+
+    parser.add_argument(
+        "--mute-stream",
+        "-M",
+        action="store_true",
+        help="Use this flag to disable the streaming StdOut callback for LLMs.",
+    )
 
     return parser.parse_args()
 
