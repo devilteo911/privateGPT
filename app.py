a62c886f6fa3292fb9d8f5082197d8cb4ad91c6b
Matteo Moro <morom@previdom.previnet.it>
2023-08-10 14:19:36 +0000

    💄 ui updates

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
+from datetime import datetime
+import json
 import streamlit as st
-from ingest import LOADER_MAPPING
+from ingest import LOADER_MAPPING, main as ingest_docs
 from routes.overloadPDF import inference
-from utils.utils import SimpleStreamlitCallbackHandler
+from utils.utils import FakeArgs, SimpleStreamlitCallbackHandler
 
 # Sidebar
-st.sidebar.title("Menu")
+st.sidebar.title("⚡ Overload PDF Chat 🤖")
+
+
+st.sidebar.warning(
+    "OverloadChat may produce inaccurate information about people, documents, date or amounts. Please always refer to the original document",
+    icon="⚠",
+)
+
+with st.expander(label="File uploader"):
+    if uploaded_files := st.sidebar.file_uploader(
+        "Upload a file",
+        # type=[x.replace(".", "") for x in LOADER_MAPPING.keys()],
+        accept_multiple_files=True,
+        label_visibility="collapsed",
+    ):
+        total_docs = len(uploaded_files)
+        if st.sidebar.button("Upload"):
+            for i, uploaded_file in enumerate(uploaded_files):
+                bytes_data = uploaded_file.read()
+                with open(f"source_documents/{uploaded_file.name}", "wb") as f:
+                    f.write(bytes_data)
+                if i == total_docs - 1:
+                    pass
+                args = FakeArgs()
+                ingest_docs(args)
 
 # Add sliders to sidebar
-remote_emb = st.sidebar.checkbox("Remote Embeddings")
-remote_model = st.sidebar.checkbox("Remote Model")
-max_tokens = st.sidebar.slider("Max Tokens", 128, 4096, step=128)
-paragraph_overlap = st.sidebar.selectbox("Paragraph Overlap", [0, 1, 2])
+st.sidebar.subheader("Remote Selection")
+col1, col2 = st.sidebar.columns(2)
+remote_emb = col1.checkbox("Embeddings")
+remote_model = col2.checkbox("Model")
+
+st.sidebar.subheader("Model Parameters")
+max_tokens = st.sidebar.slider("Max Tokens", 128, 4096, step=128, value=4096)
+paragraph_overlap = st.sidebar.slider("Paragraph Overlap", 0, 2, step=1, value=1)
 
 params = {
     "remote_emb": remote_emb,
@@ -20,24 +50,6 @@ params = {
 }
 
 # Main content
-st.title("⚡ Overload PDF Chat 🤖")
-
-uploaded_files = st.file_uploader(
-    "Upload a file",
-    type=[x.replace(".", "") for x in LOADER_MAPPING.keys()],
-    accept_multiple_files=True,
-)
-
-total_docs = len(uploaded_files)
-if st.button("Upload"):
-    for i, uploaded_file in enumerate(uploaded_files):
-        bytes_data = uploaded_file.read()
-        with open(f"source_documents/{uploaded_file.name}", "wb") as f:
-            f.write(bytes_data)
-        # if i == total_docs - 1:
-        #     pass
-        # args = FakeArgs()
-        # ingest_docs(args)
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -57,10 +69,11 @@ if prompt := st.chat_input("Write your question here..."):
         st.markdown(prompt)
     # Display assistant response in chat message container
     assistant = st.chat_message("assistant")
-    full_response = inference(
-        {"query": prompt, "params": params},
-        callbacks=[SimpleStreamlitCallbackHandler(message_area=assistant)],
-    )
+    with st.spinner("Thinking..."):
+        full_response = inference(
+            {"query": prompt, "params": params},
+            callbacks=[SimpleStreamlitCallbackHandler(message_area=assistant)],
+        )
 
     if assistant:
         with st.expander("Document Similarity Search"):
@@ -75,3 +88,13 @@ if prompt := st.chat_input("Write your question here..."):
             "docs": full_response["docs"],
         }
     )
+
+col1, col2 = st.sidebar.columns(2)
+
+
+if clear_chat := col1.button("Clear chat"):
+    st.session_state.messages = []
+
+if save_chat := col2.button("Save chat"):
+    with open(f"logs/chat_history_{datetime.now()}.json", "w") as f:
+        json.dump(st.session_state.messages, f)
